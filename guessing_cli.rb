d45abdf0f3c53b29334a7aaed9ca7372c492054f
Michael Soares <mikey.soares@gmail.com>
2019-01-14 01:20:34 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
 if gets == 'exit'
   puts "Goodbye!"
-elsif gets == true
+elsif gets == rand
   puts "You guessed the correct number!"
 end
 
