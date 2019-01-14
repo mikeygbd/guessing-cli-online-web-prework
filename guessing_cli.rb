5d68911fad949635d22ad45cb894270af9ebd1c6
Michael Soares <mikey.soares@gmail.com>
2019-01-14 01:39:33 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -11,7 +11,9 @@ def run_guessing_game
     else
       puts "The computer guessed #{number}."
     end
+    puts "Guess a number between 1 and 6."
+    input = gets.chomp
   end
-
+    puts "Goodbye"
 
 end
